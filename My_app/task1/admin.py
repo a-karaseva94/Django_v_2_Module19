007b935be46599217879fdf2189dc7ca2a3c16e2
a-karaseva94 <a-karaseva94@mail.ru>
2024-12-03 18:09:03 +0300

    Домашнее задание по теме "Сайт администрирования" (Карасева А.Д.)

--- My_app/task1/admin.py
+++ My_app/task1/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Админка для Buyer
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+
+
+# Админка для Games
+@admin.register(Games)
+class GamesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'description', )
+    search_fields = ('title', )
+    list_filter = ('cost', )
+    list_per_page = 5  # количество игр на странице
+    # Разбивка на секции
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'cost', 'description')
+        }),
+
+        ('Дополнительные настройки', {
+            'classes': ('collapse',), # скрытие секции по умолчанию
+            'fields': ('size', 'age_limited')
+        }),
+    )
+
